dde7068c831c06b0db0a015dc8cb73e6ec0f8596
Aditya Kumar <106132841+Aditya-567@users.noreply.github.com>
2023-09-16 01:21:47 +0530

    Create 1991. Find the Middle Index in Array.cpp

---
+++ 1991. Find the Middle Index in Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n = nums.size();
+        int leftsum = 0 , rightsum = 0 , prefix = 0;
+        for(int i = 0 ; i < n ; i++){
+            prefix += nums[i];
+        }
+        rightsum  = prefix ; 
+        for(int j= 0 ; j<n ; j++){
+            leftsum += nums[j]; 
+            if(rightsum == leftsum) {
+              return j;
+            }
+            rightsum -= nums[j];
+        }
+        return -1;
+    }
+};
